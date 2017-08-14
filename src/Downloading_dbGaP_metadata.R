ddd1a313a16fd7e671c3d46b3cc32b6b98d01416
David McGaughey <mcgaughey@gmail.com>
2017-08-14 17:02:25 -0400

    made more general

--- src/Downloading_dbGaP_metadata.R
+++ src/Downloading_dbGaP_metadata.R
@@ -2,6 +2,7 @@
 #dbGaP tools, Lunning made this: https://github.com/jameslhao/dbgapr_dev
 library('dbgapr')
 c <- Commons()
-prjConfig(c, prjDir="/Users/michellemiron/Desktop/dbGaPR")
+prjConfig(c, prjDir="~/dbGaPR")
 getPrjDir(c, showErr = TRUE)
 ftpDownload(c, dataStudyOnly=F)
+
