dba0422ecf0d8e390e8dfafa5bb7d88173ab4b4a
David McGaughey <mcgaughey@gmail.com>
2017-08-14 17:01:30 -0400

    note added

--- src/sraDB_pull.R
+++ src/sraDB_pull.R
@@ -2,6 +2,7 @@
 library(SRAdb)
 library(tidyverse)
 
+print('Make sure you have >30GB of free drive space!')
 if(!file.exists('~/SRAmetadb.sqlite')) sqlfile <<- getSRAdbFile(destdir='~/',destfile='SRAmetadb.sqlite.gz')
 sqlfile = '~/SRAmetadb.sqlite'
 sra_con <- dbConnect(SQLite(),sqlfile)
