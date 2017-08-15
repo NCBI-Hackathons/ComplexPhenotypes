e3d47c49a62d17827c7453e545ada35ba7b81a1b
David McGaughey <mcgaughey@gmail.com>
2017-08-15 17:17:47 -0400

    typos

--- dbGaPdb/R/pull_dbGapdb_sqlite.R
+++ dbGaPdb/R/pull_dbGapdb_sqlite.R
@@ -1,13 +1,14 @@
 #' Downloads dbGaPdb sqlite from temp link and unzips it
 #'
-#' @importsFrom R.Utils gunzip
+#' @import R.Utils
 #' @export
 pull_dbGaPdb_sqlite <- function(local_path=tempdir(),
-                                sqlite_db_name='dbGaPdb.sqlite.gz',
+                                sqlite_db_name='dbGaPdb.sqlite',
                                 link='https://helix.nih.gov/~mcgaugheyd/dbGapdb.sqlite.gz'){
   path_and_file_gz <- paste0(local_path, '/', sqlite_db_name, '.gz')
   path_and_file <- paste0(local_path, '/', sqlite_db_name)
   download.file(link, path_and_file_gz)
-  gunzip(path_and_file_gz)
+  print('Downloaded, now unzipping')
+  R.utils::gunzip(path_and_file_gz)
   return(path_and_file)
 }
