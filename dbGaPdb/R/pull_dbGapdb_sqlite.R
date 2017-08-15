67def73c5923f2f89004d7a0613e29a0a24d3738
David McGaughey <mcgaughey@gmail.com>
2017-08-15 17:17:33 -0400

    typos

--- dbGaPdb/R/pull_dbGapdb_sqlite.R
+++ dbGaPdb/R/pull_dbGapdb_sqlite.R
@@ -8,6 +8,6 @@ pull_dbGaPdb_sqlite <- function(local_path=tempdir(),
   path_and_file_gz <- paste0(local_path, '/', sqlite_db_name, '.gz')
   path_and_file <- paste0(local_path, '/', sqlite_db_name)
   download.file(link, path_and_file_gz)
-  R.utils::gunzip(path_and_file_gz)
+  gunzip(path_and_file_gz)
   return(path_and_file)
 }
