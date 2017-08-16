6486d3a83aa86211d0c9e3e80cc5991d36f081c9
michellemiron <miron.michelle@gmail.com>
2017-08-16 08:50:03 -0400

    plotting

--- UseCase_1_MergeWithSRA.R
+++ UseCase_1_MergeWithSRA.R
@@ -1,2 +1,11 @@
 #Use Case Number 1: Cross reference this GaP database with SRA
       #Find what GaP studies exist in terms of their data types categorized in SRA
+
+#Make a graph like this, this one has fake data:
+test <-dat <- read.table(text = "A   B   C   D   E   F    G
+ 1 480 780 431 295 670 360  190
+ 2 720 350 377 255 340 615  345
+ 3 460 480 179 560  60 735 1260
+ 4 220 240 876 789 820 100   75", header = TRUE)
+barplot(as.matrix(test), main="Car Distribution",horiz=TRUE,
+           names.arg=c("3 Gears", "4 Gears", "5 Gears", "r","r","r","dew"))
