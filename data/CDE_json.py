c17043381bee192d364464a0f053e114cdc2ce49
rbutleriii <rbutler1@hawk.iit.edu>
2017-08-16 01:41:25 -0400

    json parse edits

--- data/CDE_json.py
+++ data/CDE_json.py
@@ -2,49 +2,49 @@
 
 import json
 import sys
+import re
 import pprint
 
-def find_dbGaPs(tree_root):
+def find_dbGaPs(tree_root, index):
     dbGaPs = {}
-    for key in tree_root['dataSets']:
-        source = tree_root['dataSets'][key]['source']
-        notes = tree_root['dataSets'][key]['notes']
+    for n in range(len(tree_root[index]['dataSets'])):
+        source = tree_root[index]['dataSets'][n]['source']
+        notes = tree_root[index]['dataSets'][n]['notes']
         phenx_id = re.search('(PX\d+)$', notes).group() or "not found"
-        id_set = tree_root['dataSets'][key]['id'] or "not found"
+        id_set = tree_root[index]['dataSets'][n]['id'] or "not found"
         if source == "dbGaP" and notes.startswith('PhenX') and id_set != "not found":
             dbGaPs[id_set] = phenx_id
     return dbGaPs
 
-
-def find_name(tree_root):
-    for key1 in tree_root['naming']:
-        for key2 in tree_root['naming'][key1]['tags']:
-            if tree_root['naming'][key1]['tags'][key2]['tag'] == 'Primary Name':
-                primary_name = tree_root['naming'][key1]['designation']
+def find_name(tree_root, index):
+    for n in range(len(tree_root[index]['naming'])):
+        for m in range(len(tree_root[index]['naming'][n]['tags'])):
+            if tree_root[index]['naming'][n]['tags'][m]['tag'] == 'Primary Name':
+                primary_name = tree_root[index]['naming'][n]['designation']
                 return primary_name
-            elif tree_root['naming'][key1]['tags'][key2]['tag'] == 'Long Common Name':
-                primary_name = tree_root['naming'][key1]['designation']
+            elif tree_root[index]['naming'][n]['tags'][m]['tag'] == 'Long Common Name':
+                primary_name = tree_root[index]['naming'][n]['designation']
                 return primary_name
-            elif tree_root['naming'][key1]['tags'][key2]['tag'] == 'Shortname':
-                primary_name = tree_root['naming'][key1]['designation']
+            elif tree_root[index]['naming'][n]['tags'][m]['tag'] == 'Shortname':
+                primary_name = tree_root[index]['naming'][n]['designation']
                 return primary_name
             else:
                 primary_name = "no name in CDE"
                 return primary_name 
 
-def find_element_tree(tree_root):
-    pass
+#def find_element_tree(tree_root):
+#    pass
 
-def output(outfile, dbGaPs, primary_name):
+def output_tsv(outfile, dbGaPs, primary_name):
     for id_set in dbGaPs:
         outfile.write(id_set + '\t' + dbGaPs[id_set] + '\t' + primary_name + '\n')
 
-tree = open(sys.argv[1])
-parsed_tree = json.load(tree)
 outfile = open("CDE_table.tsv", "w")
 outfile.write('phv_id\tPhenX_id\tCDE_primary_name\n')
-for key in parsed_tree:
-    output(outfile, find_dbGaPs(parsed_tree[key]), find_name(parsed_tree[key])
+tree = open(sys.argv[1])
+parsed_tree = json.load(tree)
+for index in range(len(parsed_tree)):
+    output_tsv(outfile, find_dbGaPs(parsed_tree, index), find_name(parsed_tree, index))
 
 #pprint.pprint(parsed_tree[0:2])
 #pprint.pprint(parsed_tree[0]['classification'][0]['elements'][0]['elements'][0]['elements'])
